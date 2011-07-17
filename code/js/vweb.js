afb8f7dd42dd79154fe4c094192ec162fb9646b4
drdrxp <drdr.xp@gmail.com>
2011-07-17 19:28:45 +0800

    add functions to namespace $ and $.fn

--- code/js/vweb.js
+++ code/js/vweb.js
@@ -46,7 +46,128 @@ $.extend( $, {
             ev.stopPropagation && ev.stopPropagation(); /* pop up                  */
             ev.preventDefault && ev.preventDefault();   /* other event on this DOM */
         }
+    },
+    simpText: function( str ) {
+        return $.trim( str ).replace( / +/g, ' ' );
+    },
+    unparam: function( s ) {
+        var o = {};
+        var args = s.split( "&" );
+        $.each( args, function( i, e ){
+            if ( e != "" ) {
+                var kv = e.split( "=" );
+                var k = decodeURIComponent( kv[ 0 ] );
+                var v = decodeURIComponent( kv[ 1 ] );
+                o[ k ] = v;
+            }
+        } );
+
+        return o;
+    },
+    unescape: function( html ) {
+        var htmlNode = document.createElement('div');
+        htmlNode.innerHTML = html;
+        if (htmlNode.innerText) {
+            return htmlNode.innerText; // IE
+        }
+        return htmlNode.textContent; // FF
     }
+} );
+
+$.extend( $.fn, {
+    to_json: function() {
+        // TODO filter non-comment node
+        var rst = [];
+        $( this ).each( function( i, v ){
+            var e = $( this );
+            var j, text;
+            if ( this.nodeType == 3 ) {
+                text = e.simpText();
+                if ( text != '' ) {
+                    rst.push( { text: this.nodeValue } );
+                }
+            }
+            else {
+                j = { node: { tag: this.tagName, id:e.id(), class: e.attr( 'class' ) } };
+                switch ( j.node.tag ) {
+                case 'IMG' :
+                    j.node.src = e.attr( 'src' );
+                    break;
+                case 'A' :
+                    j.node.href = e.attr( 'href' );
+                    break;
+                }
+                var children = e.contents();
+                if ( children.length > 0 ) {
+                    j.children = children.to_json();
+                }
+                rst.push( j );
+            }
+        } );
+
+        return rst;
+    },
+    offset_tl: function(){
+        var tl = $(this).offset();
+        return { t:tl.top, l:tl.left };
+    },
+    _outerSize: function( funcname, withMargin ){
+        withMargin = withMargin == undefined ? true : withMargin;
+        var s = 0;
+        this.each( function( i, v ){
+            s += $(v)[ funcname ]( withMargin );
+        } );
+        return s;
+    },
+    h: function( withMargin ) {
+        return this._outerSize( 'outerHeight', withMargin );
+    },
+    w: function( withMargin ) {
+        return this._outerSize( 'outerWidth', withMargin );
+    },
+    size_wh: function( withMargin ) {
+        return { w: this.w( withMargin ), h: this.h( withMargin ) };
+    },
+    id: function() { return this.attr( 'id' ); },
+    simpText: function() {
+        return $.simpText( this.text() );
+    },
+    simpVal: function() {
+        return $.simpText( this.val() );
+    },
+    btn_opt: function (  ) {
+        var e = $( this );
+        var opt = {
+            icons : {},
+            text : e.attr( "_text" ) != "no"
+        };
+
+        e.attr( "_icon" ) && ( opt.icons.primary = "ui-icon-" + e.attr( "_icon" ) );
+        e.attr( "_icon2" ) && ( opt.icons.secondary = "ui-icon-" + e.attr( "_icon2" ) );
+
+        return opt;
+    },
+    jsonRequest: function( succ ){
+
+        $( this ).each( function(){
+
+            var form = $( this );
+
+            $.ajax( {
+                url : form.attr( 'action' ),
+                type : form.attr( 'method' ),
+                data : form.serialize(),
+                dataType : 'json',
+                success : succ
+            } );
+
+        } );
+    }
+} );
+
+$.extend( $.fn, {
+    tl: $.fn.offset_tl,
+    p: $.fn.parents
 } );
 
 $.extend( $.vweb, {
@@ -216,8 +337,10 @@ $.extend( $.vweb, {
     init_sub: function( self ) {
         $.each( self, function( k, v ){
             var u = self[ k ];
-            u._elt = $( "#" + k );
-            u.init && u.init( u._elt );
+            if ( u.init ) {
+                u._elt = $( "#" + k );
+                u.init( u._elt );
+            }
         } );
     }
 } );
@@ -318,6 +441,7 @@ $.extend( $.vweb.ui, {
 
         $( container ).delegate( ".t_msg .imgwrap", "click", function(){
             var e = $( this ).p( ".t_msg" );
+            $.log( e + 'clicked' );
             e.toggleClass( 'thumb' );
             e.toggleClass( 'midpic' );
         } );
@@ -930,8 +1054,11 @@ $.extend( $.vweb.ui.tree, {
 
 } );
 
+
+
 $.extend( $.vweb.ui.t.list, {
     init : function () {
+        $.log( "setup list" );
         this.last = {};
         $.vweb.ui.setup_img_switch( this._elt.empty() );
         this.setup_func();
@@ -1123,6 +1250,7 @@ $.extend( $.vweb.ui.t.paging, {
 
 $.extend( $.vweb.ui.t, {
     init: function (){
+        $.log( 'init $.vweb.ui.t' );
         $.vweb.init_sub( this );
     }
 } );
@@ -1254,130 +1382,9 @@ $.extend( $.vweb.ui.t.my.globalsearch, {
     }
 } );
 
-$.unparam = function( s ){
-    var o = {};
-    var args = s.split( "&" );
-    $.each( args, function( i, e ){
-        if ( e != "" ) {
-            var kv = e.split( "=" );
-            var k = decodeURIComponent( kv[ 0 ] );
-            var v = decodeURIComponent( kv[ 1 ] );
-            o[ k ] = v;
-        }
-    } );
-
-    return o;
-}
 
 
-$.unescape = function(html) {
-    var htmlNode = document.createElement('div');
-    htmlNode.innerHTML = html;
-    if (htmlNode.innerText) {
-        return htmlNode.innerText; // IE
-    }
-    return htmlNode.textContent; // FF
-}
 
-$.fn.to_json = function() {
-    // TODO filter non-comment node
-    var rst = [];
-    $( this ).each( function( i, v ){
-        var e = $( this );
-        var j, text;
-        if ( this.nodeType == 3 ) {
-            text = e.simpText();
-            if ( text != '' ) {
-                rst.push( { text: this.nodeValue } );
-            }
-        }
-        else {
-            j = { node: { tag: this.tagName, id:e.id(), class: e.attr( 'class' ) } };
-            switch ( j.node.tag ) {
-            case 'IMG' :
-                j.node.src = e.attr( 'src' );
-                break;
-            case 'A' :
-                j.node.href = e.attr( 'href' );
-                break;
-            }
-            var children = e.contents();
-            if ( children.length > 0 ) {
-                j.children = children.to_json();
-            }
-            rst.push( j );
-        }
-    } );
-
-    return rst;
-}
-
-$.fn.tl = $.fn.offset_tl = function(){
-    var tl = $(this).offset();
-    return { t:tl.top, l:tl.left };
-}
-
-$.fn._outerSize = function( funcname, withMargin ){
-    withMargin = withMargin == undefined ? true : withMargin;
-    var s = 0;
-    this.each( function( i, v ){
-        s += $(v)[ funcname ]( withMargin );
-    } );
-    return s;
-}
-$.fn.h = function( withMargin ) {
-    return this._outerSize( 'outerHeight', withMargin );
-}
-
-$.fn.w = function( withMargin ) {
-    return this._outerSize( 'outerWidth', withMargin );
-}
-$.fn.size_wh = function( withMargin ) {
-    return { w: this.w( withMargin ), h: this.h( withMargin ) };
-}
-
-$.fn.p = $.fn.parents;
-
-$.fn.id = function() { return this.attr( 'id' ); }
-
-$.simpText = function( str ) {
-    return $.trim( str ).replace( / +/g, ' ' );
-}
-$.fn.simpText = function() {
-    return $.simpText( this.text() );
-}
-$.fn.simpVal = function() {
-    return $.simpText( this.val() );
-}
-
-$.fn.btn_opt = function (  ) {
-    var e = $( this );
-    var opt = {
-        icons : {},
-        text : e.attr( "_text" ) != "no"
-    };
-
-    e.attr( "_icon" ) && ( opt.icons.primary = "ui-icon-" + e.attr( "_icon" ) );
-    e.attr( "_icon2" ) && ( opt.icons.secondary = "ui-icon-" + e.attr( "_icon2" ) );
-
-    return opt;
-}
-$.fn.jsonRequest = function( succ ){
-
-    $( this ).each( function(){
-
-        var form = $( this );
-
-        $.ajax( {
-            url : form.attr( 'action' ),
-            type : form.attr( 'method' ),
-            data : form.serialize(),
-            dataType : 'json',
-            success : succ
-        } );
-
-    } );
-}
 
 $( function() {
     if ( MODE == 'album' ) {
