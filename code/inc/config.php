454b35880423956718f282c6dfad8164504fea7b
drdr xp <drdr.xp@gmail.com>
2011-10-16 22:55:37 +0800

    fix

--- code/inc/config.php
+++ code/inc/config.php
@@ -11,6 +11,8 @@ define( "VWEB_VD_SEC", "05ee2c6350a2ed432c7b41c06f9089e1" );
 define( "LINK_VWEB", "http://t.cn/a0yUgu" );
 define( "LINK_VWEB_DEV", "http://t.cn/aOXV5H" );
 
+define( "ROOT", _SERVER["DOCUMENT_ROOT"] )
+
 // vweb.sinaapp.com
 // http://t.cn/a0yUgu
 
