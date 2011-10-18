a844e663fa2eba900dc88a60128af0e5fb266371
drdr xp <drdr.xp@gmail.com>
2011-10-18 19:45:06 +0800

    add localfscache

---
+++ code/service/localfscache/localfscache.class.php
@@ -0,0 +1,31 @@
+<?
+
+class LocalFsCache
+{
+    $pref = "any";
+
+    function __construct( $pref ) {
+        $this->pref = $pref;
+    }
+
+    function _key( $key ) {
+        return $this->pref . ":$key";
+    }
+
+    function path( $key ) {
+        return SAE_TMP_PATH . $this->_key( $key );
+    }
+
+    function read( $key ) {
+        $localfn = $this->path( $key );
+        return file_get_contents( $localfn );
+    }
+
+    function write( $key, $cont ) {
+        $localfn = $this->path( $key );
+        $r = file_put_contents( $localfn, $cont );
+        return $r;
+    }
+}
+
+?>
