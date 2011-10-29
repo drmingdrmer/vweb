d8930aed5684b27d9c6d691f88ae5a5580c81621
drdr xp <drdr.xp@gmail.com>
2011-10-29 23:03:32 +0800

    support logging engine switch

--- code/inc/debug.php
+++ code/inc/debug.php
@@ -1,76 +1,86 @@
 <?
 
-$levels = array(
-    'debug'=>10,
-    'info'=>8,
-    'ok'=>6,
-    'warn'=>4,
-    'error'=>2,
-);
+class PageLog {
+    static public function write( $lvl, $msg ) {
+        $frm = Logging::_frame();
+        echo "<span style='color:#999;width:400px;display:inline-block;'> $frm </span>";
 
-$_debugLevel = 'debug';
-// $_debugLevel = 'warn';
+        echo "$msg<br/>\n";
+        ob_flush();
+        flush();
+    }
+}
+
+class Logging {
+
+    static public $levels = array(
+        'debug' => 10,
+        'info'  => 8,
+        'ok'    => 6,
+        'warn'  => 4,
+        'error' => 2,
+    );
+
+    static public $level = 10;
+
+    static public $engine = PageLog;
+
+    static public function set_engine( $e ) {
+        Logging::$engine = $e;
+    }
+
+    static public function set_level( $lvl ) {
+        Logging::$level = Logging::$levels[ $lvl ];
+    }
+
+    static public function do_log( $lvl, $msg ) {
+        $LVL = strtoupper( $lvl );
+        if ( Logging::$level >= Logging::$levels[ $lvl ] ) {
+            $e = Logging::$engine;
+            echo "engine is :" . print_r( $e, true );
+            $e::write( Logging::$levels[ $lvl ], "[ $LVL ] $msg" );
+        }
+    }
+
+    static public function _frame() {
+
+        $bts = debug_backtrace();
+
+        do {
+            $bt = array_shift($bts);
+            $fn = $bt[ 'file' ];
+        } while ( $fn == __FILE__ );
+
+
+        $line = $bt[ 'line' ];
+        $fn = $bt[ 'file' ];
+        $fn = substr( $fn, strlen( $_SERVER[ "DOCUMENT_ROOT" ] ) );
+
+
+        $bt = array_shift( $bts );
+
+        $object = $bt['object'];
+        if (is_object($object)) {
+             $clz = get_class($object);
+        }
+        else {
+            $clz = '';
+        }
+
+        $now = strftime("%H:%M:%S");
+        return "$now [ $fn:$line $clz ]";
+    }
+}
+
+function dd( $msg ) { Logging::do_log( 'debug', $msg ); }
+function dinfo( $msg ) { Logging::do_log( 'info', $msg ); }
+function dok( $msg ) { Logging::do_log( 'ok', $msg ); }
+function dwarn( $msg ) { Logging::do_log( 'warn', $msg ); }
+function derror( $msg ) { Logging::do_log( 'error', $msg ); }
+
 
 if ( $_GET[ 'level' ] ) {
-    $_debugLevel = $_GET[ 'level' ];
+    Logging::set_level( $_GET[ 'level' ] );
 }
-
-$_debugLevel = $levels[ $_debugLevel ];
-
-$_debugFn = '.';
-
-function _pos() {
-    $bts = debug_backtrace();
-    array_shift($bts);
-    array_shift($bts);
-    array_shift($bts);
-    $bt = array_shift($bts);
-
-    $line = $bt[ 'line' ];
-    $fn = $bt[ 'file' ];
-    $fn = substr( $fn, strlen( $_SERVER[ "DOCUMENT_ROOT" ] ) );
-
-
-    $bt = array_shift( $bts );
-
-    $object = $bt['object'];
-    if (is_object($object)) {
-         $clz = get_class($object);
-    }
-    else {
-        $clz = '';
-    }
-
-    $now = strftime("%H:%M:%S");
-    echo "<span style='color:#999;width:400px;display:inline-block;'>$now [ $fn:$line $clz ]</span>";
-}
-
-function _write( $msg ) {
-    echo "$msg<br/>\n";
-    ob_flush();
-    flush();
-}
-
-function _write_pos( $msg ) {
-    _pos();
-    _write( $msg );
-}
-
-function if_log( $lvl ) {
-    global $_debugLevel;
-    global $levels;
-    return $_debugLevel >= $levels[ $lvl ];
-}
-
-function _write_pos_if( $lvl, $msg ) {
-    $LVL = strtoupper( $lvl );
-    if_log( $lvl ) && _write_pos( "[ $LVL ] $msg" );
-}
-
-function dd( $msg ) { _write_pos_if( 'debug', $msg ); }
-function dinfo( $msg ) { _write_pos_if( 'info', $msg ); }
-function dok( $msg ) { _write_pos_if( 'ok', $msg ); }
-function dwarn( $msg ) { _write_pos_if( 'warn', $msg ); }
-function derror( $msg ) { _write_pos_if( 'error', $msg ); }
 
 ?>
